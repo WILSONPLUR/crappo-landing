28510892cffe0cdf975d072ffed34093b8ee63bf
whoheywhat <oldschoolmoi@gmail.com>
2022-05-18 13:51:47 +0300

    crappo-v1

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const menuIcon = document.getElementById("header__menu-icon");
+const headerMenu = document.getElementById("menu");
+const headerMenuLinks = document.querySelectorAll(".header__link");
+
+menuIcon.addEventListener("click", (e) => {
+  if (headerMenu.style.display === "none") {
+    headerMenu.style.display = "block";
+    menuIcon.style.display = "none";
+  } else {
+    headerMenu.style.display = "none";
+  }
+});
+
+headerMenuLinks.forEach((headerMenuLink) => {
+  headerMenuLink.addEventListener("click", (e) => {
+    headerMenu.style.display = "none";
+    menuIcon.style.display = "block";
+  });
+});
